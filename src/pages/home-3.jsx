4855826fe9f1c7881a9a3a3422b159f806391f2a
kawsarkabir <devkawsarkabir@gmail.com>
2024-07-09 14:19:05 +0600

    home3- plan area done

--- src/pages/home-3.jsx
+++ src/pages/home-3.jsx
@@ -1,6 +1,7 @@
 import Digital_product_Home_3 from '../components/digital-product-home-3'
 import Marquee_Area_Home_3 from '../components/marquee-area-home-3'
 import Offer_Area_Home_3 from '../components/offer-area-home-3'
+import Plan_Area_Home_3 from '../components/plan-area-home-3'
 import Services_Area_Home_2 from '../components/services-area-home-2'
 import Footer from '../components/shared/footer'
 import StartHeroThree from '../components/start-hero-3'
@@ -13,6 +14,7 @@ const HomeThree = () => {
             <Marquee_Area_Home_3 />
             <Services_Area_Home_2 />
             <Digital_product_Home_3 />
+            <Plan_Area_Home_3 />
             <Footer />
         </>
     )
