defcebecb2e9910be8b03f7f21d985779fa8ab14
Anondo Bormon <117563820+devanondo@users.noreply.github.com>
2024-07-10 09:06:33 +0600

    [HOME 2]

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,11 +1,16 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import HomeOne from '../pages/home-1'
+import HomeTwo from '../pages/home-2'
 
 const Routes = () => {
     const router = createBrowserRouter([
         {
             path: '/',
             element: <HomeOne />,
+        },
+        {
+            path: '/home-2',
+            element: <HomeTwo />,
         },
     ])
 
