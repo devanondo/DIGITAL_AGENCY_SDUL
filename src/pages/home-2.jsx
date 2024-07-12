6c59df51a524569ca273d0616051250c2d8a1c08
Anondo Bormon <117563820+devanondo@users.noreply.github.com>
2024-07-12 20:44:33 +0600

    navigation

--- src/pages/home-2.jsx
+++ src/pages/home-2.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import StartHeroTwo from '../components/start-hero-2'
-import StartSeoHomeTwo from '../components/start-seo-home-2'
-import Navbar from '../components/shared/navbar'
-import Footer from '../components/shared/footer'
+import BlogHome2 from '../components/blog-home-2'
 import BrandLogo from '../components/brand-logo'
-import VideoPopupHomeTwo from '../components/video-popup-home-2'
+import FaqHome2 from '../components/faq-home-2'
 import MarketingAreaHome2 from '../components/marketing-area-home-2'
 import MarqueAreaHome2 from '../components/marque-area-home-2'
 import RecentWorkHomeTwo from '../components/recent-work-home-2'
-import FaqHome2 from '../components/faq-home-2'
-import TeamHome2 from '../components/team-home-2'
+import Footer2 from '../components/shared/footer2'
+import Navbar from '../components/shared/navbar'
+import StartHeroTwo from '../components/start-hero-2'
+import StartSeoHomeTwo from '../components/start-seo-home-2'
 import SubscribeHome2 from '../components/subscribe-home-2'
-import BlogHome2 from '../components/blog-home-2'
+import TeamHome2 from '../components/team-home-2'
+import VideoPopupHomeTwo from '../components/video-popup-home-2'
 
 const HomeTwo = () => {
     return (
@@ -29,7 +29,7 @@ const HomeTwo = () => {
             <SubscribeHome2 />
             <BlogHome2 />
 
-            <Footer />
+            <Footer2 />
         </>
     )
 }
